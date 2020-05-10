84e5b3dc611d46f9af4d63e09d65a1e52b440d58
Fatih Guclu Akkaya <gucluakkaya@gmail.com>
2020-05-10 15:35:44 +0200

    implement JWK endpoint

---
+++ server/src/main/kotlin/com/opencarrental/authorizationservice/configuration/JwkSetEndpointConfiguration.kt
@@ -0,0 +1,4 @@
+package com.opencarrental.authorizationservice.configuration
+
+class JwkSetEndpointConfiguration {
+}
---
+++ server/src/main/kotlin/com/opencarrental/authorizationservice/controller/JwkSetController.kt
@@ -0,0 +1,4 @@
+package com.opencarrental.authorizationservice.controller
+
+class JwkSetController {
+}
